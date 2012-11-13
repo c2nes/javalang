55559ed06ebaf9a139ef0734b5ea418f1c7528a4
Chris Thunes <cthunes@brewtab.com>
2012-11-13 14:20:37 -0500

    Add distutils support.

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+from distutils.core import setup
+
+setup(
+    name = "javalang",
+    packages = ["javalang"],
+    version = "0.9.0",
+    author = "Chris Thunes",
+    author_email = "cthunes@brewtab.com",
+    url = "http://github.com/c2nes/pylangtools",
+    description = "Pure Python Java parser and tools",
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries"
+        ],
+    long_description = """\
+========
+javalang
+========
+
+javalang is a pure Python library for working with Java source
+code. javalang provies a lexer and parser targeting Java 7. The
+implementation is based on the Java language spec available at
+http://docs.oracle.com/javase/specs/jls/se7/html/.
+
+"""
+)
