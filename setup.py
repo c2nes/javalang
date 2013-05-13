ed2bd2d5f2b2e70f4ad11513e3c1fe177117e261
Chris Thunes <cthunes@brewtab.com>
2013-05-13 09:20:39 -0400

    Version 0.9.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name = "javalang",
     packages = ["javalang"],
-    version = "0.9.3",
+    version = "0.9.4",
     author = "Chris Thunes",
     author_email = "cthunes@brewtab.com",
     url = "http://github.com/c2nes/javalang",
