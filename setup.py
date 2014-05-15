8f6ccacef153c70a18d36811aa95691f86997378
David Black <dblack@atlassian.com>
2014-05-15 15:28:54 +1000

    add 'six' to tests_require in setup.py.
    Signed-off-by: David Black <dblack@atlassian.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 
+install_requires = ["six",]
+tests_require = install_requires[:] + ["nose",]
+
 setup(
     name = "javalang",
     packages = ["javalang"],
@@ -29,7 +32,7 @@ http://docs.oracle.com/javase/specs/jls/se7/html/.
 
 """,
     zip_safe = False,
-    install_requires = ['six',],
-    tests_require = ["nose",],
+    install_requires = install_requires,
+    tests_require = tests_require,
     test_suite = "nose.collector",
 )
